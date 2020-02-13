83e1cfe1b31502b4d94f5b6993b67b5749b16ba1
Shawn Yin <yin.shuo@husky.neu.edu>
2020-02-13 15:53:34 -0800

    Update debugcharts.go

--- debugcharts.go
+++ debugcharts.go
@@ -29,7 +29,7 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
-	"github.com/mkevac/debugcharts/bindata"
+	"github.com/mkevac/yinfirefire/bindata"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/process"
 )
